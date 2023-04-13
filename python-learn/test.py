c6cd462e57f6392412c23ae7d25159d2bb202198
XiChenYang61 <1835039483@qq.com>
2023-04-13 13:16:21 +0800

    测试github练习代码

---
+++ python-learn/test.py
@@ -0,0 +1,4 @@
+print("键盘敲烂")
+print("月薪过万")
+print("就来黑马程序员")
+hello
---
+++ test4_11.py
@@ -0,0 +1,10 @@
+"""九九乘法表"""
+
+i = 1
+while i <= 9:
+    j = 1
+    while j <= i:
+        print(j,"*",i,"=",j * i,end=" ")
+        j += 1
+    print()
+    i += 1
